e2706503d0627a2fc975077e739b7fb755f66026
HILLELWIE_DAHLIN <HILLEWIE.DAHLIN@LIVE.SE>
2021-03-11 16:55:25 +0100

    saved

--- src/components/MovieAPI.js
+++ src/components/MovieAPI.js
@@ -17,9 +17,10 @@ import MovieList from './MovieList';
             setMovies(data.Search); 
         };
     } 
-        useEffect(() => {
-            FetchMovies(searchValue);
-        }, [searchValue]);
+    
+    useEffect(() => {
+        FetchMovies(searchValue);
+    }, [searchValue]);
  
 
 
@@ -41,81 +42,7 @@ import MovieList from './MovieList';
             </div>
     )
 }
-  
-// export class MovieAPI extends Component {
-//     constructor(props) {
-//         super(props);
-    
-//         this.state = {
-//             userInput: '',
-//             movies: []
-//         };
-//     }
-        
-//     fetchMovies = async () => {
-//         try {
-//             const url = `http://www.omdbapi.com/?s=Harry&apikey=df23ef2`;
-            
-//             const response = await fetch(url);
-//             const data = await response.json();
-//             console.log(data);
 
-//             const fetchUrl = [
-//                 ...this
-//             ]
-
-//             this.setState({
-//                 movies: data
-//             })
-            
-            
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-
-
-//     searchMovie = (newSearch) => {
-//         const searchResult = [
-//             ...this.state.movies,
-//             newSearch
-//         ]
-
-//         this.setState({
-//             movies: searchResult
-//         })
-//     }
-
-//   render() {
-//       const { movies } = this.state; 
-      
-//     return (
-//         <div>
-//             <Form 
-//                  searchMovie={this.searchMovie}
-//             />
-
-//             <MovieList movies={this.movies} />
-
-// <section>
-
-// {movies.map((movie) => (
-//         <article key={movies.id}>
-//         <h2>{movies.Title}</h2>
-//          </article>
-// ))}
-// </section>
-       
-
-            
-//             <h1>Movies</h1>
-//             <button onClick={this.fetchMovies}>Movies</button>
-            
-        
-//         </div>
-//         )
-//     }
-// }
 export default MovieAPI 
 
 
